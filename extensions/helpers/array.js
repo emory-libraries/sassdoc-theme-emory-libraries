d94d637c4e158947d5087fae3f3477aa2350ec15
Lauren Hamel <lauren.hamel@outlook.com>
2019-02-18 11:26:00 -0500

    Adds a handlebars `where` helper for easily extracting objects from an array of objects by key-value pair

--- extensions/helpers/array.js
+++ extensions/helpers/array.js
@@ -15,6 +15,54 @@ module.exports = {
     
     return result;
     
+  },
+  
+  where( array, key, value, comparator ) {
+    
+    // Set the comparator.
+    if( typeof comparator != 'string' ) comparator = '==';
+    
+    // Get keys.
+    const keys = key.split('.');
+    
+    // Find object items.
+    const objects = array.filter((item) => typeof item == 'object' && !Array.isArray(item) && item !== null);
+   
+    // Ignore arrays without objects.
+    if( objects.length === 0 ) return [];
+    
+    // Find objects with key-value pairs.
+    const matches = objects.filter((object) => {
+      
+      // Initialize pointer.
+      let pointer = object;
+      
+      // Move pointer.
+      for( let i = 0; i < keys.length; i++ ) {
+        
+        // Move the pointer if the key exists.
+        if( pointer[keys[i]] ) pointer = pointer[keys[i]];
+        
+        // Otherwise, ignore the object.
+        else return false;
+        
+      }
+
+      // Compare the pointer and value.
+      switch( comparator ) {
+        case '===': return pointer === value;
+        case '>': return pointer > value;
+        case '>=': return pointer >= value;
+        case '<': return pointer < value;
+        case '<=': return pointer <= value;
+        default: return pointer == value;
+      }
+      
+    });
+   
+    // Return matches.
+    return matches;
+    
   }
   
 };
